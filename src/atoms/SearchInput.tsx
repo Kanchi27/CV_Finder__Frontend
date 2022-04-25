8d53a7d14fd0921915d346d9ee7462b1d97788ea
kdora <kanchan.dora@here.com>
2022-04-25 19:44:54 +0530

    Cv Finder Frontend code

--- src/atoms/SearchInput.tsx
+++ src/atoms/SearchInput.tsx
@@ -22,7 +22,7 @@ export const SearchInput: FC<ISearchInput> = ({ placeholder, type, onSubmit }: I
         <div className="mb-3 xl:w-96 max-w-xs">
             <div className="input-group relative flex items-stretch w-full mb-4">
                 <input type={type}
-                    ref={inputRef} pattern="[a-zA-Z1-9\s]+"
+                    ref={inputRef}
                     className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                     placeholder={placeholder} aria-label="Search" aria-describedby="button-addon2" />
                 <button
